ebdd2a2b3adef3ba2d9eb9d79239525eac694f78
alex <alexreveles@yahooo.com>
2021-04-20 16:59:29 -0700

    new update with code

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
     entry: './assets/js/script.js', output: {
@@ -11,6 +12,10 @@ module.exports = {
     $: "jquery",
     jQuery: "jquery"
   }),
+  new BundleAnalyzerPlugin({
+    analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+  })
+
 ],
   mode: 'development'
 };
