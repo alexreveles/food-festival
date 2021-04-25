c8be62c57a4a423348e0e80ef93d7a2d3f54783c
alex <alexreveles@yahooo.com>
2021-04-24 21:42:49 -0700

    updated code

--- service-worker.js
+++ service-worker.js
@@ -47,21 +47,43 @@ const FILES_TO_CACHE = [
   });
 
 
-  // Respond with cached resources  
-  self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-      caches.match(e.request).then(function (request) {
-        if (request) { // if cache is available, respond with cache
-          console.log('responding with cache : ' + e.request.url)
-          return request
-        } else {       // if there are no cache, try fetching request
-          console.log('file is not cached, fetching : ' + e.request.url)
-          return fetch(e.request)
-        }
-  
-        // You can omit if/else for console.log & put one line below like this too.
-        // return request || fetch(e.request)
-      })
-    )
-  })
+  // Intercept fetch requests
+self.addEventListener('fetch', function(e) {
+    if (e.request.url.includes('/api/')) {
+        e.respondWith(
+          caches
+            .open(DATA_CACHE_NAME)
+            .then(cache => {
+              return fetch(e.request)
+                .then(response => {
+                  // If the response was good, clone it and store it in the cache.
+                  if (response.status === 200) {
+                    cache.put(e.request.url, response.clone());
+                  }
+    
+                  return response;
+                })
+                .catch(err => {
+                  // Network request failed, try to get it from the cache.
+                  return cache.match(e.request);
+                });
+            })
+            .catch(err => console.log(err))
+        );
+    
+        return;
+      }
+
+      e.respondWith(
+        fetch(e.request).catch(function() {
+          return caches.match(e.request).then(function(response) {
+            if (response) {
+              return response;
+            } else if (e.request.headers.get('accept').includes('text/html')) {
+              // return the cached home page for all requests for html pages
+              return caches.match('/');
+            }
+          });
+        })
+      );
+});
